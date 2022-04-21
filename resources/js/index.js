f54913a008a13fa96db2e4e9c13dc0918005cee3
Isaac Elijah <devilliersisaac@gmail.com>
2022-04-21 16:26:12 -0400

    Time with interval JS

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,14 @@
 const codingTime = document.getElementById('codeTime');
 let currentDate = new Date();
-let cDay = currentDate.getDay();
-let cMonth = currentDate.getMonth() + 1;
-let cYear = currentDate.getFullYear();
-console.log(cDay + ' ' + cMonth + ' ' + cYear);
+let pDate = new Date("01/01/2015");
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+
+
+setInterval(() => {
+    const now = new Date();
+    const diff = Math.floor((now.getTime() - pDate.getTime()) / 1000);
+    codingTime.innerText = intlNumberFormatter.format(diff);
+}, 1000);
+
+setInterval();
